57c042b39a843f17f2a09afb10703e45b018f0b6
rhedayati <rhedayatiuofc@gmail.com>
2024-10-16 11:53:53 -0600

    earl_on_lable added

--- late_calculator.py
+++ late_calculator.py
@@ -180,7 +180,7 @@ def main():
     
     try:
         deadline, zip_file_name, csv_file_name, grade_book_flag, course_name, assignment_name, personal_days_column, late_window, filter_label, early_label_on, output_format = get_user_inputs(yaml_file)
-        # extract_zip(zip_file_name)
+        extract_zip(zip_file_name)
         submissions = parse_folder_names()
         late_submissions = calculate_late_submissions(submissions, deadline, late_window, early_label_on)
         generate_output(late_submissions, course_name, assignment_name, output_format)
